0fe85fd3672e7c60b1bd06dec06d3ca611ddd140
Akash <akasharul2101@gmail.com>
2020-10-19 22:29:07 +0530

    get post and create post over

--- nodeServer/_models/post.js
+++ nodeServer/_models/post.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, Types } = require("mongoose");
 
 const Post = new Schema({
     username: {
@@ -8,19 +8,34 @@ const Post = new Schema({
     title: {
         type: String,
         required: true,
-        maxlength: 255
+        maxlength: 255,
+        minlength: 5
     },
     body: {
         type: String,
         required: true,
         minlength: 10
     },
-    numlikes: Number,
-    numcomments: Number,
-
+    numlikes: {
+        type: Number,
+        default: 0
+    },
+    numcomments: {
+        type: Number,
+        default: 0
+    }
 }, { timestamps: true })
 
-const Likes = new Schema({
+const Like = new Schema({
+    postId: {
+        type: Types.ObjectId,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    }
 })
 
-module.exports = model('Post', Post);
+module.exports.Post = model('Post', Post);
+module.exports.Like = model('Like', Like)
--- nodeServer/routes/discussionforum.js
+++ nodeServer/routes/discussionforum.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const authorise = require('../_middlewares/auth');
+const { Post } = require('../_models/post');
 
 const forumRouter = express.Router();
 
 forumRouter.post('/:username', authorise, async (req, res) => {
-    let newpost = req.body.post;
-    res.json({ message: "this will store posts in the database"})
+    const { title, body } = req.body;
+    const username = req.params.username;
+
+    let post = new Post({
+        username, title, body
+    })
+    await post.save();
+    res.json({ message: "created new post successfully !"})
 })
 
 forumRouter.get("/:username", authorise, async (req, res) => {
-    res.json({message: "these will be populated with public posts"});
+    await Post.find({ username: req.params.username }, (err, data) => {
+        res.json(data);
+    })
 })
 
 module.exports = forumRouter;
