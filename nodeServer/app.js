67ba49b5088fe4021a44ffe1f389840d9f2039f8
Akash <akasharul2101@gmail.com>
2020-10-26 17:03:22 +0530

    Add process to cwd to static file dir

--- nodeServer/app.js
+++ nodeServer/app.js
@@ -23,7 +23,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // add middlewares
-app.use(express.static('/home/akash/Desktop/IEA/nodeServer/dist/angular-front-end'));
+app.use(express.static('./dist/angular-front-end'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -38,7 +38,7 @@ app.use('/api/forum', forumRouter);
 app.use('/api/profile', profileRouter);
 app.use('/api/mail', mailRouter);
 app.get("*", (req, res) => {
-  res.sendFile("./dist/angular-front-end/index.html", {root: "/home/akash/Desktop/IEA/nodeServer"});
+  res.sendFile("./dist/angular-front-end/index.html", {root: process.cwd()});
 })
 
 // catch 404 and forward to error handler
